24b9bbc55dc442376adc0f2e5ebd47904181300e
Yonatan Ninio <yonatan.n@proton.me>
2026-03-10 12:54:25 +0200

    dragging works

--- Assets/Scripts/ColorPicker/SVBoxClickHandler.cs
+++ Assets/Scripts/ColorPicker/SVBoxClickHandler.cs
@@ -2,11 +2,22 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using System;
 
-public class SVBoxClickHandler : MonoBehaviour, IPointerClickHandler
+public class SVBoxClickHandler : MonoBehaviour, IPointerClickHandler, IPointerDownHandler, IDragHandler
+
 {
     public Action<Vector2> OnClick;
 
     public void OnPointerClick(PointerEventData eventData)
+    {
+        OnClick?.Invoke(eventData.position);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        OnClick?.Invoke(eventData.position);
+    }
+
+    public void OnDrag(PointerEventData eventData)
     {
         OnClick?.Invoke(eventData.position);
     }
